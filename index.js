e88be60145f585c05616d66c05348814b163ce9a
gloria <gloria.gao115@gmail.com>
2023-02-28 01:30:02 +1100

    commit tree 1cc8e4c4c67daebb67f52bd1c9b9d23a11f6dcc6

--- index.js
+++ index.js
@@ -11,7 +11,30 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// console.log(tutorials.map(item => item))
+// const titleCased = tutorials.map(function (tutorials) {
+//   return tutorials.replace(tutorials[0], tutorials[0].toUpperCase())
+// })
+// console.log(newArr)
+// // return newArr
+// return tutorials.map(function (tutorials) {
+//   return tutorials.replace(tutorials[0], tutorials[0].toUpperCase())
+
+
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
+  tutorials.map(tutorials => tutorials.replace(tutorials[0], tutorials[0].toUpperCase()))
+
   return tutorials
 }
+// console.log(newArr)
+// return newArr
+
+
+
+// const rollCall = students.map(function(student) {
+//   return student + " the wizard";
+
+
+// titleCased();
+console.log(titleCased)
